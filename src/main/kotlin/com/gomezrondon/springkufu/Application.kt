b625f5ee86a3347b293ac1df9d4927037ca1937e
cajgomez <Jgomez@cambridgeassociates.com>
2019-10-19 10:12:34 -0600

    adding a second endpoint.

--- src/main/kotlin/com/gomezrondon/springkufu/Application.kt
+++ src/main/kotlin/com/gomezrondon/springkufu/Application.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 import javax.persistence.Entity
@@ -19,13 +20,9 @@ fun main(args: Array<String>) {
 @RestController
 class PersonController(private val personRepository: PersonRepository) {
 	@GetMapping("/person")
-	fun readAll(): MutableIterable<Person> {
-		return personRepository.findAll()
-	}
-	@GetMapping("/person2")
-	fun readAll2(): MutableIterable<Person> {
-		return personRepository.findAll()
-	}
+	fun readAll() = personRepository.findAll()
+	@GetMapping("/person/{id}")
+	fun readAll2(@PathVariable id:Long) = personRepository.findById(id)
 }
 
 
