b23aedb6c1afdb24c816e4b4a423878f111dfe67
cajgomez <Jgomez@cambridgeassociates.com>
2019-10-19 10:50:03 -0600

    Adding functional routing and http test request.

--- src/main/kotlin/com/gomezrondon/springkufu/Application.kt
+++ src/main/kotlin/com/gomezrondon/springkufu/Application.kt
@@ -2,29 +2,49 @@ package com.gomezrondon.springkufu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Service
+import org.springframework.web.servlet.function.HandlerFunction
+import org.springframework.web.servlet.function.RequestPredicates.*
+import org.springframework.web.servlet.function.RouterFunction
+import org.springframework.web.servlet.function.RouterFunctions.nest
+import org.springframework.web.servlet.function.RouterFunctions.route
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
 import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @SpringBootApplication
-class Application
+class Application{
+
+	@Bean
+	fun routes(handler: PersonHandler): RouterFunction<ServerResponse> {
+
+		return nest(
+				path("/person"),
+				route(
+						GET("/{id}"),
+						HandlerFunction { handler.readOne(it) })
+						.andRoute(method(HttpMethod.GET),
+								HandlerFunction { handler.readAll(it) })
+		)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
 }
 
-@RestController
-class PersonController(private val personRepository: PersonRepository) {
-	@GetMapping("/person")
-	fun readAll() = personRepository.findAll()
-	@GetMapping("/person/{id}")
-	fun readAll2(@PathVariable id:Long) = personRepository.findById(id)
+@Service
+class PersonHandler(private val repository: PersonRepository){
+	fun readAll(request:ServerRequest):ServerResponse = ServerResponse.ok().body(repository.findAll())
+	fun readOne(request:ServerRequest):ServerResponse = ServerResponse.ok().body(
+			repository.findById(request.pathVariable("id").toLong())
+	)
 }
-
 
 interface PersonRepository: PagingAndSortingRepository<Person, Long>
 
